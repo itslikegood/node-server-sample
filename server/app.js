3c110ae41ebd7d19b29961ced0eb7d65803e4e66
Joey Vargas <itslikegood@gmail.com>
2016-03-15 20:26:55 -0700

    trying out google cloud with node

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
+var gcloud     = require('gcloud')({ projectId: 'dailynervedev' });
 
 var app = express();
+var gcs = gcloud.storage();
 
-app.use(bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/info', function(req, res) {
